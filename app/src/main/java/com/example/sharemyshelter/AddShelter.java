15a01f70596e70aaaec6a8cb703dbfeb972726c2
regevino <dan.regev@mail.huji.ac.il>
2021-05-21 02:46:22 +0300

    1

--- app/src/main/java/com/example/sharemyshelter/AddShelter.java
+++ app/src/main/java/com/example/sharemyshelter/AddShelter.java
@@ -3,6 +3,7 @@ package com.example.sharemyshelter;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -42,6 +43,7 @@ public class AddShelter extends AppCompatActivity {
                 longitude.setText("");
                 latitude.setText("");
                 address.setText("");
+                Toast.makeText(AddShelter.this, "Shelter added successfully", Toast.LENGTH_LONG).show();
             }
         });
 //        Geocoder geocoder = new Geocoder(this);
--- app/src/main/java/com/example/sharemyshelter/RedColor.java
+++ app/src/main/java/com/example/sharemyshelter/RedColor.java
@@ -2,8 +2,12 @@ package com.example.sharemyshelter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class RedColor extends AppCompatActivity {
@@ -17,6 +21,19 @@ public class RedColor extends AppCompatActivity {
 
         TextView alertTextView = findViewById(R.id.alertTextView);
         TextView shelterInfo = findViewById(R.id.shelterInfoTextView);
+        Button navigateButton = findViewById(R.id.navigateButton);
+
+        navigateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse("google.navigation:q=רחוב+יוסי+בן+יועזר+41+ירושלים&mode=w");
+                // Uri gmmIntentUri = Uri.parse("google.navigation:geo:31.7768,-122.4192&mode=w");
+
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
 
         alertTextView.setText("RUN!!");
         Shelter shelter = (Shelter) (getIntent().getSerializableExtra("shelter"));
